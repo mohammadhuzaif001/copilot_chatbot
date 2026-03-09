faea4aa03dfced23eb7da92302b7bd34052f5c61
Your Name <your.email@example.com>
2026-03-09 12:55:48 +0530

    token update

---
+++ ai.py
@@ -0,0 +1,27 @@
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-4.1"
+#token = os.environ["GITHUB_TOKEN"]
+token="add your token"
+
+client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(token),
+)
+
+response = client.complete(
+    messages=[
+        SystemMessage(""),
+        UserMessage("What is the capital of France?"),
+    ],
+    temperature=1,
+    top_p=1,
+    model=model
+)
+
+print(response.choices[0].message.content)
+
